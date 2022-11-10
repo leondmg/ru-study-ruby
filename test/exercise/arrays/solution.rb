b75515241a2964216b0e3c66d559f837bb5f1f15
Dmitry Leontev <dmitriy1leontev@gmail.com>
2022-11-10 13:19:06 +0300

    Add replace function

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,20 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = find_max(array)
+        array.map { |elem| elem.positive? ? max : elem }
       end
 
       def search(_array, _query)
         0
+      end
+
+      private
+
+      def find_max(array)
+        max = array[0]
+        array.each { |elem| max = elem if elem > max }
+        max
       end
     end
   end
